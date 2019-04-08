ce7b87badeddeb9a18524f5a8a4c1d19444b2e89
MrGio7 <lashagiorgi1233@gmail.com>
2019-04-09 00:20:13 +0400

    updated Social Card header container visual effects in header.css

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -4,7 +4,9 @@ import './Header.css';
 
 const ImageThumbnail = () => {
     return (
-        <img className="logo" alt="Logo of the lambda school" src="https://tk-assets.lambdaschool.com/1c1b7262-cf23-4a9f-90b6-da0d3c74a5c6_lambdacrest.png"></img>
+        <div className="logo">
+            <img alt="Logo of the lambda school" src="https://tk-assets.lambdaschool.com/1c1b7262-cf23-4a9f-90b6-da0d3c74a5c6_lambdacrest.png"></img>
+        </div>
     )
 }
 
