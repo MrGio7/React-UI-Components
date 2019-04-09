785f2b7f5c113323874f3ddd1859ba51766c1593
MrGio7 <lashagiorgi1233@gmail.com>
2019-04-09 20:30:44 +0400

    updated calculator visual structure

--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,46 @@
 import React from 'react';
 import './Button.css';
+import ActionButton from './ActionButton';
+
+function renderActionButton(i) {
+    return <ActionButton value={i} />
+};
+
+const NumberButton = () =>{
+    return(
+        <div className='buttons'>   
+        <div className="board-row">
+        {renderActionButton('clear')}
+        {renderActionButton('÷')}
+        </div>
+
+        <div className="board-row">
+        {renderActionButton(7)}
+        {renderActionButton(8)}
+        {renderActionButton(9)}
+        {renderActionButton('X')}
+        </div>
+
+        <div className="board-row">
+        {renderActionButton(4)}
+        {renderActionButton(5)}
+        {renderActionButton(6)}
+        {renderActionButton('-')}
+        </div>
+
+        <div className="board-row">
+        {renderActionButton(1)}
+        {renderActionButton(2)}
+        {renderActionButton(3)}
+        {renderActionButton('+')}
+        </div>
+
+        <div className="board-row">
+        {renderActionButton(0)}
+        {renderActionButton('=')}
+        </div>
+        </div>
+    )
+}
+
+export default NumberButton;
--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Display.css';
+
+const CalculatorDisplay = () => {
+    return(
+        <div className='display'>
+            <input className='textview'></input>
+        </div>
+    )
+}
+
+export default CalculatorDisplay;
